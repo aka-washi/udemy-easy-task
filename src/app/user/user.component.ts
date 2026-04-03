b820631644ed5d76fdf00fe24f7c053997b88f8c
Jesus Lara <jesus.lara@alight.com>
2026-04-03 15:57:13 +0000

    traditional approach

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,11 +7,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
-
-  onSelectUser(): void {
-    this.selectedUser.set(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  
+  get imagePath(): string {
+    return `assets/users/${this.avatar}`;
   }
+
+
+  onSelectUser(): void {}
 
 }
