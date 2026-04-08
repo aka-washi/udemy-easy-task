f61a366a3ceaf6e280062ee5378146fcac45ad7f
Jesus Lara <jesus.lara@alight.com>
2026-04-08 20:54:31 +0000

    dependency injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,28 +1,28 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-import { dummyTasks } from './../dummy-tasks';
+import { TaskComponent } from './task/task.component';
 import { type Task, type NewTaskData } from './task/task.model';
 import { NewTaskComponent } from './new-task/new-task.component';
-
+import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddingTask = false;
-  tasks = dummyTasks;
+
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter(task => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onCompleteTask(task: Task) {
-    this.tasks = this.tasks.filter(t => t.id !== task.id);
+    this.tasksService.removeTask(task.id);
   }
 
   onStartAddTask() {
@@ -34,16 +34,7 @@ export class TasksComponent {
   }
 
   onAddTasks(taskData: NewTaskData) {
-    const newTask: Task = {
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    };
-    this.tasks.push(newTask);
+    this.tasksService.addTask(taskData, this.userId);
     this.isAddingTask = false;
   }
-
-
 }
