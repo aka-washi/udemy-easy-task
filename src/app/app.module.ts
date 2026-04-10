be5fa28ef9091943d1f7df120a5a09ec3d2e96ff
Jesus Lara <jesus.lara@alight.com>
2026-04-10 23:08:43 +0000

    migrating all components to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    CardComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent] // for standalone components
+  imports: [BrowserModule, FormsModule] // for standalone components
 })
 export class AppModule {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
-import { CardComponent } from '../shared/card/card.component';
 
 //type User = { id: string; avatar: string; name: string };
 
 @Component({
   selector: 'app-user',
-  standalone: true,
+  standalone: false,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
-  imports: [CardComponent]
+  styleUrl: './user.component.css'
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
